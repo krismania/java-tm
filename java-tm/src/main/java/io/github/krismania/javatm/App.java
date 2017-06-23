37077a57e281602d3e5536aa35e77b5a07e55a91
Kristian Giglia <krismania1@gmail.com>
2017-06-23 17:30:46 +1000

    Add command line arg support

--- java-tm/src/main/java/io/github/krismania/javatm/App.java
+++ java-tm/src/main/java/io/github/krismania/javatm/App.java
@@ -1,25 +1,42 @@
 package io.github.krismania.javatm;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import com.google.gson.Gson;
 
 public class App 
 {
-    public static void main(String[] args) throws FileNotFoundException
+    public static void main(String[] args)
     {
-    	FileReader fr = new FileReader(new File("../incrementor.json"));
-        Gson gson = new Gson();
-        
-        // load TM from json
-        TuringMachine tm = gson.fromJson(fr, TuringMachine.class);     
-        System.out.println(tm);
-        
-        // initialize the tape
-        tm.initTape("10100111");
-        
-        // execute machine
-        tm.execute();
+    	if (args.length == 2)
+    	{
+    		String tmFile = args[0];
+    		String initialTape = args[1];
+    		
+    		try (InputStreamReader file = new InputStreamReader(new FileInputStream(tmFile), "UTF-8"))
+    		{
+    			Gson gson = new Gson();
+    			
+    			// load TM from json
+    	        TuringMachine tm = gson.fromJson(file, TuringMachine.class);     
+    	        System.out.println(tm);
+    	        
+    	        // initialize the tape
+    	        tm.initTape(initialTape);
+    	        
+    	        // execute machine
+    	        tm.execute();
+    		}
+    		catch (IOException e)
+    		{
+    			System.out.println("There was an error opening the file.");
+    		}
+    	}
+    	else
+    	{
+    		System.out.println("Please supply the path to a TM Json and an initial tape.");
+    	}
     }
 }
